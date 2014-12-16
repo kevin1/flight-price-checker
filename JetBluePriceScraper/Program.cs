60f9bb325c43daca924970b4cb3b962219f0bd2c
Kevin Chen <kevin@kevinchen.co>
2014-12-16 00:31:39 -0500

    Initial commit.
    Empty project + code formatting settings

---
+++ JetBluePriceScraper/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace JetBluePriceScraper
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+        }
+    }
+}
