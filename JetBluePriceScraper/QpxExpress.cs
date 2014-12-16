04535405b5eb4e61455883d28d5f3895167c050c
Kevin Chen <kevin@kevinchen.co>
2014-12-16 02:11:25 -0500

    Add JSON.NET library

---
+++ JetBluePriceScraper/QpxExpress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Json;
+
+namespace JetBluePriceScraper
+{
+    public static partial class FlightPrices
+    {
+        public Search(string[] origins, string[] destinations, string[] airlines, DateTime departDate)
+        {
+
+        }
+    }
+}
+
---
+++ JetBluePriceScraper/QpxExpressApiKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JetBluePriceScraper
+{
+    public class QpxExpressApiKey
+    {
+        public QpxExpressApiKey()
+        {
+        }
+    }
+}
+
